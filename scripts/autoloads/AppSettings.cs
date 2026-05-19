55c7b9ae9a735725157f36895cc932e46d12e553
hvergi <4914651+hvergi@users.noreply.github.com>
2026-05-19 01:36:25 -0400

    0.2.1 start

--- scripts/autoloads/AppSettings.cs
+++ scripts/autoloads/AppSettings.cs
@@ -8,7 +8,7 @@ public static class AppSettings
     private const string SavePath = "user://settings.json";
 
     public static string GitHubRepo { get; } = "hvergi/hvergi-toolkit";
-    public static string CurrentVersion { get; } = "0.2.0";
+    public static string CurrentVersion { get; } = "0.2.1";
 
     public class AlertSettings
     {
